604c97424ff89f198524c4396e85ce6746b615fb
Joel Swensen <joelswensen@gmail.com>
2020-04-15 12:09:13 -0700

    added mongoDB functionality

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const _ = require("lodash");
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+mongoose.connect("mongodb+srv://Onyx_Admin:Minda@1998@cluster0-ll84a.mongodb.net/todolistDB", {
   useUnifiedTopology: true,
   useNewUrlParser: true
 });
@@ -25,6 +25,12 @@ const itemsSchema = new mongoose.Schema({
 
 const Item = new mongoose.model("Item", itemsSchema);
 
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("list", listSchema);
 
 app.get("/", function(req, res) {
 
@@ -38,39 +44,95 @@ app.get("/", function(req, res) {
   })
 });
 
+app.get("/:customListName", function(req, res){
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({name: customListName}, function(err, foundList){
+    if(!err){
+      if(!foundList){
+        const list = new List({
+          name: customListName,
+          items:[]
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      }else{
+        res.render("list",{listTitle: foundList.name, newListItems: foundList.items});
+      }
+    }
+  });
+});
+
+
+
 app.post("/", function(req, res) {
 
   const itemName = req.body.newItem;
+  const listName = req.body.list;
 
   const item = new Item({
     name: itemName
   });
-  item.save();
-  res.redirect("/");
 
-});
-
-app.get("/work", function(req, res) {
-  res.render("list", {
-    listTitle: "Work List",
-    newListItems: workItems
-  });
-});
-
-app.get("/about", function(req, res) {
-  res.render("about");
-});
-
-app.post("/delete", function(req,res){
-  const checkedItemId = req.body.checkbox
-  Item.findByIdAndDelete(checkedItemId, function(err){
-  if(!err){
-    console.log("Item deleted")
+  if(listName === "Today"){
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({name: listName}, function(err, foundList){
+      let x = foundList.items;
+      x.push(item);
+      //for some reason foundList.items.push(item)
+      //was giving the error "can't push to undefined. using a mid let (x) solved it"
+      foundList.save();
+      res.redirect("/" + listName);
+    })
   }
-  res.redirect("/")
-  })
 
-})
+
+});
+
+// app.get("/work", function(req, res) {
+//   res.render("list", {
+//     listTitle: "Work List",
+//     newListItems: workItems
+//   });
+// });
+
+// app.get("/lists", function(req,res){
+//   res.render("lists");
+// })
+
+// app.get("/lists/:newList/", function(req,res){
+//   const listName = req.params.newList;
+//   console.log(listName);
+//   res.render("list", {
+//     listTitle: listName
+//   })
+//
+// })
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if (!err) {
+        console.log("Successfully deleted checked item.");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+      if (!err){
+        res.redirect("/" + listName);
+      }
+    });
+  }
+
+
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
